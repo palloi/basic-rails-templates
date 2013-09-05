41ec42c890b2a2a98b0fcdde877a6f7c5d0c6edf
Leonard Hofmann <leonard.hofmann@locaweb.com.br>
2013-09-05 14:35:21 -0300

    add rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BasicRailsTemplates::Application.config.secret_key_base = '10b202ae8e3271942c8d314e4503ec8230622594e7a2a262b9ab391e9369942ea23e8d288cb239573b0de08ca7231c4468cb9355e4be1d90eddde059a7d68cdb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BasicRailsTemplates::Application.config.session_store :cookie_store, key: '_basic-rails-templates_session'
