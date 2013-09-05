e643579f8bf665345a1ab34b1f134574ea199eca
Leonard Hofmann <leonard.hofmann@locaweb.com.br>
2013-09-05 16:44:19 -0300

    start basic template, application.html with content for js

---
+++ app/assets/javascripts/basic.js
@@ -0,0 +1,8 @@
+var basic = {
+	init: function () {
+		// Sintax Highlight do Google Prettify
+    $('pre').addClass('prettyprint linenums');
+    
+    prettyPrint();
+	}
+};
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 BasicRailsTemplates::Application.routes.draw do
+  root 'home#index'
+  get '/basic', to: 'home#basic'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
